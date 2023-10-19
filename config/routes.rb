66cfa95cf6ec7fed6a55a0a9ad06f830d6f53614
farhan <farhanj.mohamed@gmail.com>
2023-10-19 13:28:31 -0700

    adding user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Todo Routes
   get "/todos" => "todos#index"
   get "/todos/:id" => "todos#show"
   post "/todos" => "todos#create"
   patch "/todos/:id" => "todos#update"
   delete "/todos/:id" => "todos#destroy"
+
+  # Users Routes
+  post "/users" => "users#create"
 end
